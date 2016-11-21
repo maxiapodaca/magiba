a6fa487a5d0e10c290cec559c156bf49a9471b81
Barbara Acosta <barbara.acosta@live.com.ar>
2016-11-21 17:11:28 -0300

    merge con Gina

--- db/migrate/20161116153335_agregar_atributos_postulaciones.rb
+++ db/migrate/20161116153335_agregar_atributos_postulaciones.rb
@@ -2,8 +2,8 @@ class AgregarAtributosPostulaciones < ActiveRecord::Migration
   def change
   	remove_column :postulations, :fecha 
   	add_column :postulations, :fecha, :date 
-  	add_column :postulations, :aceptar , :boolean , default: false
-  	add_column :postulations, :noaceptar , :boolean , default: true
+  	add_column :postulations, :aceptar, :boolean, default: false
+  	add_column :postulations, :noaceptar, :boolean , default: true
   	add_column :postulations, :comentario , :string
   end
 end
